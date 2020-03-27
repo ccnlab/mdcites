172e0fe21bc983313ca82568377db7aaef5cdb82
Randall C. O'Reilly <randy.oreilly@colorado.edu>
2020-03-27 01:13:36 -0700

    mdcites markdown citation processor works instantly -- python was taking forever to load ccnlab.bib file.

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mdcites
+
 Markdown citation extractor, reads pandoc-citeproc citations, exports .bib file with those.
+
+The markdown citation format is `[@RefYY]` and the RefYY is the bibtex cite key that is looked up in the source .bib file
+
+```
+Usage of mdcites:
+  -bib string
+    	required full path to .bib file containing all references that could be cited
+  -dir string
+    	optional directory containing .md files to process (default "./")
+  -out string
+    	filename for output .bib file containing only the cited references (default "references.bib")
+   
+ ```
+
+
---
+++ mdcites.go
@@ -0,0 +1,111 @@
+// Copyright (c) 2020, The CCNLab Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// mdcites extracts markdown citations in the format [@Ref] from .md files and
+// looks those up in a specified .bib file, and writes the refs to target .bib
+// file which can then be used by pandoc-citeproc to efficiently process
+// references.
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"regexp"
+
+	"github.com/goki/ki/dirs"
+	"github.com/goki/pi/langs/bibtex"
+)
+
+func main() {
+	var srcDir string
+	var srcBib string
+	var outBib string
+	flag.StringVar(&srcDir, "dir", "./", "optional directory containing .md files to process")
+	flag.StringVar(&srcBib, "bib", "", "required full path to .bib file containing all references that could be cited")
+	flag.StringVar(&outBib, "out", "references.bib", "filename for output .bib file containing only the cited references")
+	flag.Parse()
+
+	if srcBib == "" {
+		flag.Usage()
+		return
+	}
+
+	exp, err := regexp.Compile(`\[@[[:alnum:]]*\]`)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	mds := dirs.ExtFileNames(srcDir, []string{".md"})
+	if len(mds) == 0 {
+		fmt.Printf("No .md files found in: %v\n", srcDir)
+		os.Exit(1)
+	}
+
+	bf, err := os.Open(srcBib)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	parsed, err := bibtex.Parse(bf)
+	if err != nil {
+		bf.Close()
+		fmt.Printf("Bibtex bibliography: %s not loaded due to error(s):\n", srcBib)
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	bf.Close()
+
+	refs := map[string]int{}
+
+	for _, md := range mds {
+		fn := filepath.Join(srcDir, md)
+		fmt.Printf("processing: %v\n", fn)
+		f, err := os.Open(fn)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		scan := bufio.NewScanner(f)
+		for scan.Scan() {
+			cs := exp.FindAllString(string(scan.Bytes()), -1)
+			if len(cs) == 0 {
+				continue
+			}
+			for _, c := range cs {
+				cc, _ := refs[c]
+				cc++
+				refs[c] = cc
+			}
+		}
+		f.Close()
+	}
+	fmt.Printf("cites:\n%v\n", refs)
+
+	ob := bibtex.NewBibTex()
+	ob.Preambles = parsed.Preambles
+	ob.StringVar = parsed.StringVar
+
+	for r, _ := range refs {
+		tr := r[2 : len(r)-1]
+		be, has := parsed.Lookup(tr)
+		if has {
+			ob.Entries = append(ob.Entries, be)
+		} else {
+			fmt.Printf("Error: Reference key: %v not found in %s\n", srcBib)
+		}
+	}
+	out := ob.PrettyString()
+
+	of, err := os.Create(outBib)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	of.WriteString(out)
+	of.Close()
+}
